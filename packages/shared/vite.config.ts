010846f25a2d27fe3ea639af6fac984d239d01c4
Folltoshe <folltoshe@foxmail.com>
2025-09-27 23:24:07 +0800

    chore(shared): add build config

---
+++ packages/shared/vite.config.ts
@@ -0,0 +1,26 @@
+import Path from 'node:path'
+
+import { defineConfig } from 'vite'
+
+import PluginDts from 'vite-plugin-dts'
+
+const RootDir = Path.join(process.cwd())
+const SourceDir = Path.join(RootDir, 'src')
+
+export default defineConfig({
+  build: {
+    outDir: Path.join(RootDir, 'dist'),
+    lib: {
+      entry: Path.join(SourceDir, 'index.ts'),
+      formats: ['es', 'cjs'],
+      fileName: 'index',
+    },
+    minify: false,
+  },
+  resolve: {
+    alias: {
+      '@music-lyric-utils/*': Path.join(RootDir, '../'),
+    },
+  },
+  plugins: [PluginDts({ rollupTypes: true })],
+})
