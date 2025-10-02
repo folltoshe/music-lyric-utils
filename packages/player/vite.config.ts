933ed855555cc27a60682c9518dbd859e7f8b62d
Folltoshe <folltoshe@foxmail.com>
2025-10-02 16:31:45 +0800

    chore(player): add build config

---
+++ packages/player/vite.config.ts
@@ -0,0 +1,24 @@
+import { join } from 'node:path'
+
+import { defineConfig } from 'vite'
+
+import PluginDts from 'vite-plugin-dts'
+
+const root = join(process.cwd())
+const src = join(root, 'src')
+
+export default defineConfig({
+  root,
+  build: {
+    outDir: join(root, 'dist'),
+    lib: {
+      entry: join(src, 'index.ts'),
+      formats: ['es', 'cjs', 'iife'],
+      name: 'MuricLyricPlayer',
+      fileName: 'index',
+    },
+    minify: false,
+    reportCompressedSize: false,
+  },
+  plugins: [PluginDts({ rollupTypes: true })],
+})
