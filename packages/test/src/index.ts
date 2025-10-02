2d3c7cfe89411c3580021b02a56bfdc46768a924
Folltoshe <folltoshe@foxmail.com>
2025-10-02 16:47:13 +0800

    feat(test): add player example

--- packages/test/src/index.ts
+++ packages/test/src/index.ts
@@ -1,5 +1,6 @@
 import Example from './example'
 import { LyricParser } from '@music-lyric-utils/parser'
+import { LyricPlayer } from '@music-lyric-utils/player'
 
 const parser = new LyricParser()
 
@@ -9,4 +10,19 @@ const result = parser.parse({
   translate: Example.translate,
 })
 
-console.log(JSON.stringify(result))
+console.log(JSON.stringify(result, null, 2))
+
+const player = new LyricPlayer({
+  offset: 0,
+  speed: 1,
+  onLinePlay(lineNum, info) {
+    console.log(`onLinePlay: ${lineNum}`)
+  },
+  onSetLyric(info) {
+    console.log(`onSetLyric`)
+  },
+})
+
+if (result) player.updateLyric(result)
+
+player.play(5000)
