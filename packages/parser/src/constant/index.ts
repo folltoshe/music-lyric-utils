fff928e57332d422de94af3ed06349cf6b5a7809
Folltoshe <folltoshe@foxmail.com>
2025-10-04 03:58:04 +0800

    feat(parser): export constant and interface from shared

--- packages/parser/src/constant/index.ts
+++ packages/parser/src/constant/index.ts
@@ -1,1 +1,6 @@
+export { EMPTY_LYRIC_INFO, EMPTY_LYRIC_LINE } from '@music-lyric-utils/shared'
+export { EMPTY_LYRIC_DYNAMIC_INFO, EMPTY_LYRIC_DYNAMIC_WORD } from '@music-lyric-utils/shared'
+
+export { LYRIC_LINE_TYPES, EMPTY_LYRIC_TIME_INFO } from '@music-lyric-utils/shared'
+
 export * from './options'
--- packages/parser/src/interface/index.ts
+++ packages/parser/src/interface/index.ts
@@ -1,1 +1,6 @@
+export type { LyricInfo, LyricLine } from '@music-lyric-utils/shared'
+export type { LyricDynamicInfo, LyricDynamicWord } from '@music-lyric-utils/shared'
+
+export type { LyricLineType, LyricTimeInfo } from '@music-lyric-utils/shared'
+
 export * from './options'
